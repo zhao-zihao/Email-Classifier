fb8d75717d74dbe9373ccdb1cc263e773e73326c
HoweZZH <zhaozihao7199@gmail.com>
2016-06-08 16:56:02 -0400

    update to fluid container

--- app/scripts/loadviews.js
+++ app/scripts/loadviews.js
@@ -2,4 +2,5 @@ $(function(){
   $("#header").load("views/header.html"); 
   $("#footer").load("views/footer.html"); 
   $('#welcome').load('views/welcome.html');
+  $('#querymodal').load('views/querymodal.html');
 });
