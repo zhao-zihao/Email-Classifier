42f0c237580c75b3a50d7ef5c488b8346420dd8b
HoweZZH <zhaozihao7199@gmail.com>
2016-06-08 21:34:14 -0400

    update about us page
    update some details of UI

--- app/scripts/loadviews.js
+++ app/scripts/loadviews.js
@@ -1,6 +1,13 @@
-$(function(){
+$(document).ready(function(){
+  console.log("document ready!");
   $("#header").load("views/header.html"); 
   $("#footer").load("views/footer.html"); 
   $('#welcome').load('views/welcome.html');
   $('#querymodal').load('views/querymodal.html');
+    
+  $('#inbox').load('views/inbox.html');
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
 });
